989a1bce7140d26442ecff0b4d420b5252a8bf6a
madew123456789 <154327057+madew123456789@users.noreply.github.com>
2024-06-03 09:54:08 +0530

    g
    g

---
+++ song.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var audio = new Audio('https://cdn.glitch.global/3e0381c4-a312-453a-a06d-22aaaffe8cb5/Sanda-Wathurak-Se-Cover-Song-Aksha-Chamudi-www.song.lk.mp3?v=1715331836989');
+    audio.play();
+};
+
+
